d8f7938bf75f8b104869ecc75f1e66341d5c1894
opaismoe <wiebe.buursink@gmail.com>
2018-03-03 15:00:48 +0100

    Add RecipeItem, Add RecipeContainer, Add Title component

---
+++ src/containers/Recipes.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import Title from '../components/Title'
+import RecipeItem from '../components/RecipeItem'
+
+class Recipes extends PureComponent {
+
+  renderRecipes(recipe, index) {
+    return <RecipeItem key={index} { ...recipe } />
+  }
+
+  render() {
+    return(
+      <div className="recipes wrapper">
+        <header>
+          <Title content="Recipes" />
+        </header>
+
+        <main>
+          { this.props.recipes.map(this.renderRecipe) }
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Recipes
